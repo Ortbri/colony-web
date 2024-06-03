7e614f301422854d0f87f4558894b96a67325fab
ortbri <brian.ort02@icloud.com>
2024-06-03 13:32:12 -0500

    Features, GetApp, & Footer

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,21 +1,23 @@
 import Image from 'next/image'
-import React from 'react'
 
 type ButtonProps = {
   type: 'button' | 'submit'
   title: string
   icon?: string
   variant: string
+  full?: boolean
 }
 
-function Button({ type, title, icon, variant }: ButtonProps) {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className='bold-16 whitespace-nowrap'>{title}</label>
+      <label className='bold-16 cursor-pointer whitespace-nowrap'>
+        {title}
+      </label>
     </button>
   )
 }
