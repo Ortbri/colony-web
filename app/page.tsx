5ab1253e7aca9cf3af202bb8d9a980bcebe9c9bb
ortbri <brian.ort02@icloud.com>
2024-06-02 20:30:21 -0500

    auto linting

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,11 @@ export default function Home() {
     <section className='py-24'>
       <div className='container'>
         <h1 className='text-3xl font-bold'>Next TS Starter</h1>
+        <h1 className='text-3xl font-bold'>Next TS Starter</h1>
+        <h3>hello there</h3>
+        <h1 className='text-3xl font-bold'>Next TS Starter</h1>
+        <h1>hello there</h1>
       </div>
-
     </section>
   )
 }
