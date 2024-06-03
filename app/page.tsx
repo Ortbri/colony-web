bc78ab5cee12d1c2a0cc9e311ad26562bc75d870
ortbri <brian.ort02@icloud.com>
2024-06-02 20:23:54 -0500

    pre-commit husky added

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-<h1 className='text-3xl font-bold'>Next TS Starter</h1>
+        <h1 className='text-3xl font-bold'>Next TS Starter</h1>
       </div>
+
     </section>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     container: {
@@ -12,10 +12,10 @@ const config: Config = {
       padding: {
         DEFAULT: '1rem',
         md: '1.5rem',
-        lg: '2rem',
+        lg: '2rem'
       }
-  }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
