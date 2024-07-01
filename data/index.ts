101047a30db3d3f08f0164fd6a7220f3792d06fe
ortbri <brian.ort02@icloud.com>
2024-07-01 13:15:09 -0500

    fix: rm about page

--- data/index.ts
+++ data/index.ts
@@ -1,11 +1,4 @@
 export const NAV_LINKS = [
-  { name: 'About', link: '/info/about' },
+  // { name: 'About', link: '/info/about' },
   { name: 'Contact Us', link: '/info/support' }
-]
-
-export const FOOTER_LINKS = [
-  {
-    title: 'Learn More',
-    links: ['Terms of use', 'Privacy Policy', 'Contact Us']
-  }
 ]
