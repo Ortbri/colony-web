77db7284a56f812ef71a2d5f8b9dc5c2b746973a
ortbri <brian.ort02@icloud.com>
2024-07-05 10:50:08 -0500

    chore:File changes

--- app/info/support/page.tsx
+++ app/info/support/page.tsx
@@ -1,11 +1,17 @@
 'use client'
 
 import React, { useState } from 'react'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
 import { FiCheck, FiClipboard } from 'react-icons/fi'
 
 export default function Support() {
-  /* ---------------------------- copy to clipboard --------------------------- */
+  /* ---------------------------------- state --------------------------------- */
   const [copied, setCopied] = useState(false)
 
   const handleCopy = () => {
@@ -15,22 +21,24 @@ export default function Support() {
   }
   /* --------------------------------- return --------------------------------- */
   return (
-    <div className='flex items-center justify-center pt-28'>
-      <Card className='border-amber-50 bg-black p-14'>
+    <div className='flex items-center justify-center py-28'>
+      <Card className='rounded-3xl border-2 bg-black px-8 py-14'>
         <CardHeader>
-          <CardTitle>Issues? Contact us</CardTitle>
+          <CardTitle>Contact us</CardTitle>
+          <CardDescription>Issues? Send an email</CardDescription>
         </CardHeader>
         <CardContent>
           <div className='flex items-center'>
-            <button onClick={handleCopy} className='mr-2'>
-              <FiClipboard className='text-white' />
-            </button>
-            <span className='text-xl'>brian@joincolonyapp.com</span>
-            {copied && (
-              <span className='ml-2'>
-                <FiCheck className='text-green-500' />
+            {copied ? (
+              <span className='ml-2 text-green-500'>
+                <FiCheck />
               </span>
+            ) : (
+              <button onClick={handleCopy} className='mr-2'>
+                <FiClipboard className='text-white' />
+              </button>
             )}
+            <span className='text-white'>brian@joincolonyapp.com</span>
           </div>
         </CardContent>
       </Card>
