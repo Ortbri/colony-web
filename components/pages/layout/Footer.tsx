d4088990d5c43225ad3bc40384919e25c25c578f
ortbri <brian.ort02@icloud.com>
2025-02-17 15:57:04 -0600

    fix:basic structure

--- components/pages/layout/Footer.tsx
+++ components/pages/layout/Footer.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear()
+
   return (
-    <footer className='mx-auto flex flex-col items-center justify-center pt-24'>
-      {/* <div className=''> */}
-      {/* <div>..</div> */}
+    <footer className='mx-auto flex w-full flex-col items-center gap-4 px-14 pb-5 pt-24'>
+      <div className='flex flex-row gap-6'>
+        <Link
+          className='group relative text-muted-foreground transition-colors hover:text-foreground'
+          href={'/'}
+        >
+          <span className='relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-foreground after:transition-all after:duration-300 group-hover:after:w-full'>
+            Privacy Policy
+          </span>
+        </Link>
+        <Link
+          className='group relative text-muted-foreground transition-colors hover:text-foreground'
+          href={'/'}
+        >
+          <span className='relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-foreground after:transition-all after:duration-300 group-hover:after:w-full'>
+            Terms of use
+          </span>
+        </Link>
+      </div>
       <p className='text-xs font-thin text-muted-foreground'>
-        © 2025 Copyright Colony. All rights reserved.
+        © {currentYear} Copyright Colony. All rights reserved.
       </p>
-      {/* <p className='text-xl'>Medium sized text here</p> */}
-      {/* <p className='text-sm'>Normal text here</p> */}
-      {/* </div> */}
     </footer>
   )
 }
--- components/pages/layout/Navbar.tsx
+++ components/pages/layout/Navbar.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <header className='flex flex-row items-center justify-between px-14 py-5'>
+    <header className='fixed left-0 right-0 top-0 z-50 flex w-full flex-row items-center justify-between bg-background/80 px-14 py-5 backdrop-blur-xl'>
       <div className='flex flex-row items-center gap-[2px]'>
         <Image
           src='/ColonyLogoWhite.png'
           alt='Join Colony Logo'
           className='invert dark:invert-0'
-          width={38}
-          height={38}
+          width={40}
+          height={40}
         />
-        <h1 className='text-xl font-semibold'>Colony</h1>
+        <h1 className='font-heading text-xl font-semibold'>Colony</h1>
       </div>
       <Button size={'sm'} className='rounded-3xl'>
         Download on iOS
