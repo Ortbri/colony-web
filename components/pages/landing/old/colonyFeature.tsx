a361070e9d791b007f5639e159fb6835d9137ae6
ortbri <brian.ort02@icloud.com>
2025-02-19 12:47:01 -0600

    fix:rest of react icons rm'd

--- components/pages/landing/old/colonyFeature.tsx
+++ components/pages/landing/old/colonyFeature.tsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import { IoShieldCheckmarkOutline } from 'react-icons/io5'
-import { MdAlternateEmail } from 'react-icons/md'
-import { GrMapLocation } from 'react-icons/gr'
+// import React from 'react'
+// import { IoShieldCheckmarkOutline } from 'react-icons/io5'
+// import { MdAlternateEmail } from 'react-icons/md'
+// import { GrMapLocation } from 'react-icons/gr'
 
-function ColonyFeature() {
-  return (
-    <div className='my-14 flex flex-col items-center'>
-      <dl className='flex w-full flex-col items-center justify-between gap-14 px-4 md:flex-row'>
-        {/* item 1 */}
-        <div className='flex w-full flex-col items-center text-center md:w-1/3'>
-          <MdAlternateEmail className='mb-4 h-12 w-12 text-white' />
-          <dt className='text-xl font-bold'>Customer Support</dt>
-          <dd className='mx-auto mt-2 flex flex-grow items-center justify-center text-lg text-gray-400'>
-            Email Brian@joincolonyapp.com
-          </dd>
-        </div>
+// function ColonyFeature() {
+//   return (
+//     <div className='my-14 flex flex-col items-center'>
+//       <dl className='flex w-full flex-col items-center justify-between gap-14 px-4 md:flex-row'>
+//         {/* item 1 */}
+//         <div className='flex w-full flex-col items-center text-center md:w-1/3'>
+//           <MdAlternateEmail className='mb-4 h-12 w-12 text-white' />
+//           <dt className='text-xl font-bold'>Customer Support</dt>
+//           <dd className='mx-auto mt-2 flex flex-grow items-center justify-center text-lg text-gray-400'>
+//             Email Brian@joincolonyapp.com
+//           </dd>
+//         </div>
 
-        {/* item 2 */}
-        <div className='flex w-full flex-col items-center text-center md:w-1/3'>
-          <GrMapLocation className='mb-4 h-12 w-12 text-white' />
-          <dt className='text-xl font-bold'>Small Town Centric</dt>
-          <dd className='mx-auto mt-2 flex flex-grow items-center justify-center text-lg text-gray-400'>
-            Focused on providing services for small towns and communities.
-          </dd>
-        </div>
+//         {/* item 2 */}
+//         <div className='flex w-full flex-col items-center text-center md:w-1/3'>
+//           <GrMapLocation className='mb-4 h-12 w-12 text-white' />
+//           <dt className='text-xl font-bold'>Small Town Centric</dt>
+//           <dd className='mx-auto mt-2 flex flex-grow items-center justify-center text-lg text-gray-400'>
+//             Focused on providing services for small towns and communities.
+//           </dd>
+//         </div>
 
-        {/* item 3 */}
-        <div className='flex w-full flex-col items-center text-center md:w-1/3'>
-          <IoShieldCheckmarkOutline className='mb-4 h-12 w-12 text-white' />
-          <dt className='text-xl font-bold'>Dispute Support</dt>
-          <dd className='mx-auto mt-2 flex flex-grow items-center justify-center text-lg text-gray-400'>
-            If job is not complete, payment will be returned
-          </dd>
-        </div>
-      </dl>
-    </div>
-  )
-}
+//         {/* item 3 */}
+//         <div className='flex w-full flex-col items-center text-center md:w-1/3'>
+//           <IoShieldCheckmarkOutline className='mb-4 h-12 w-12 text-white' />
+//           <dt className='text-xl font-bold'>Dispute Support</dt>
+//           <dd className='mx-auto mt-2 flex flex-grow items-center justify-center text-lg text-gray-400'>
+//             If job is not complete, payment will be returned
+//           </dd>
+//         </div>
+//       </dl>
+//     </div>
+//   )
+// }
 
-export default ColonyFeature
+// export default ColonyFeature
--- components/pages/landing/old/workerFeature.tsx
+++ components/pages/landing/old/workerFeature.tsx
@@ -1,113 +1,113 @@
-import { CardGrid, CardGridItem } from '@/components/cards/gridCard'
-import React from 'react'
-import Image from 'next/image'
-import { IoMdCheckmark } from 'react-icons/io'
+// import { CardGrid, CardGridItem } from '@/components/cards/gridCard'
+// import React from 'react'
+// import Image from 'next/image'
+// import { IoMdCheckmark } from 'react-icons/io'
 
-function WorkerFeature() {
-  return (
-    <div className='flex flex-col items-center justify-center'>
-      <CardGrid className=''>
-        {WorkerGridData.map((data) => (
-          <CardGridItem
-            key={data.id}
-            item={data.item}
-            className={data.className}
-          />
-        ))}
-      </CardGrid>
-    </div>
-  )
-}
+// function WorkerFeature() {
+//   return (
+//     <div className='flex flex-col items-center justify-center'>
+//       <CardGrid className=''>
+//         {WorkerGridData.map((data) => (
+//           <CardGridItem
+//             key={data.id}
+//             item={data.item}
+//             className={data.className}
+//           />
+//         ))}
+//       </CardGrid>
+//     </div>
+//   )
+// }
 
-/* --------------------------------- skelly --------------------------------- */
-const JobsSkelly = () => (
-  <div className='flex h-full w-full overflow-hidden rounded-lg'>
-    <div className='relative h-full w-full'>
-      <Image
-        src='/JobPost.png'
-        alt='Screen showing the details of a job'
-        fill
-        style={{ objectFit: 'contain', objectPosition: 'bottom center' }}
-        className='rounded-lg'
-      />
-    </div>
-  </div>
-)
-const RequestJobSkelly = () => (
-  <div className='flex h-full w-full overflow-hidden rounded-lg'>
-    <div className='relative h-full w-full'>
-      <Image
-        src='/RequestJob.png'
-        alt='Screen showing the request to apply for a job'
-        fill
-        style={{ objectFit: 'contain', objectPosition: 'bottom center' }}
-        className='rounded-lg'
-      />
-    </div>
-  </div>
-)
-const CompleteJobSkelly = () => (
-  <div className='-mb-14 flex h-full w-full overflow-hidden rounded-lg'>
-    <div className='relative h-full w-full'>
-      <Image
-        src='/CompleteJob.png'
-        alt='Screen showing a worker marking a job as complete with a photo'
-        fill
-        style={{ objectFit: 'contain', objectPosition: 'bottom center' }}
-        className='rounded-lg'
-      />
-    </div>
-  </div>
-)
+// /* --------------------------------- skelly --------------------------------- */
+// const JobsSkelly = () => (
+//   <div className='flex h-full w-full overflow-hidden rounded-lg'>
+//     <div className='relative h-full w-full'>
+//       <Image
+//         src='/JobPost.png'
+//         alt='Screen showing the details of a job'
+//         fill
+//         style={{ objectFit: 'contain', objectPosition: 'bottom center' }}
+//         className='rounded-lg'
+//       />
+//     </div>
+//   </div>
+// )
+// const RequestJobSkelly = () => (
+//   <div className='flex h-full w-full overflow-hidden rounded-lg'>
+//     <div className='relative h-full w-full'>
+//       <Image
+//         src='/RequestJob.png'
+//         alt='Screen showing the request to apply for a job'
+//         fill
+//         style={{ objectFit: 'contain', objectPosition: 'bottom center' }}
+//         className='rounded-lg'
+//       />
+//     </div>
+//   </div>
+// )
+// const CompleteJobSkelly = () => (
+//   <div className='-mb-14 flex h-full w-full overflow-hidden rounded-lg'>
+//     <div className='relative h-full w-full'>
+//       <Image
+//         src='/CompleteJob.png'
+//         alt='Screen showing a worker marking a job as complete with a photo'
+//         fill
+//         style={{ objectFit: 'contain', objectPosition: 'bottom center' }}
+//         className='rounded-lg'
+//       />
+//     </div>
+//   </div>
+// )
 
-const CompleteSkelly = () => {
-  return (
-    <div className='my-24 flex flex-col items-center'>
-      <IoMdCheckmark size={100} />
-      <div className='mx-14 flex flex-col items-center gap-6 text-center'>
-        <h3 className='text-4xl font-bold'>Complete</h3>
-        <p>Extra income. One task at a time.</p>
-      </div>
-    </div>
-  )
-}
+// const CompleteSkelly = () => {
+//   return (
+//     <div className='my-24 flex flex-col items-center'>
+//       <IoMdCheckmark size={100} />
+//       <div className='mx-14 flex flex-col items-center gap-6 text-center'>
+//         <h3 className='text-4xl font-bold'>Complete</h3>
+//         <p>Extra income. One task at a time.</p>
+//       </div>
+//     </div>
+//   )
+// }
 
-/* -------------------------------- grid data ------------------------------- */
-const WorkerGridData = [
-  {
-    id: 1,
-    item: {
-      title: 'Find a Job',
-      description: 'Get Notified when jobs are near you',
-      content: <JobsSkelly />
-    },
-    className: 'col-span-6 md:col-span-6'
-  },
-  {
-    id: 2,
-    item: {
-      title: 'Request Jobs',
-      description: 'Send requests for jobs you are interested in.',
-      content: <RequestJobSkelly />
-    },
-    className: 'col-span-6 md:col-span-3'
-  },
-  {
-    id: 3,
-    item: {
-      title: 'Complete Jobs & Receive Payment',
-      description: 'Keep track of job progress and completion.',
-      content: <CompleteJobSkelly />
-    },
-    className: 'col-span-6 md:col-span-3'
-  },
-  {
-    id: 4,
-    item: {
-      content: <CompleteSkelly />
-    },
-    className: 'col-span-6 md:col-span-6'
-  }
-]
+// /* -------------------------------- grid data ------------------------------- */
+// const WorkerGridData = [
+//   {
+//     id: 1,
+//     item: {
+//       title: 'Find a Job',
+//       description: 'Get Notified when jobs are near you',
+//       content: <JobsSkelly />
+//     },
+//     className: 'col-span-6 md:col-span-6'
+//   },
+//   {
+//     id: 2,
+//     item: {
+//       title: 'Request Jobs',
+//       description: 'Send requests for jobs you are interested in.',
+//       content: <RequestJobSkelly />
+//     },
+//     className: 'col-span-6 md:col-span-3'
+//   },
+//   {
+//     id: 3,
+//     item: {
+//       title: 'Complete Jobs & Receive Payment',
+//       description: 'Keep track of job progress and completion.',
+//       content: <CompleteJobSkelly />
+//     },
+//     className: 'col-span-6 md:col-span-3'
+//   },
+//   {
+//     id: 4,
+//     item: {
+//       content: <CompleteSkelly />
+//     },
+//     className: 'col-span-6 md:col-span-6'
+//   }
+// ]
 
-export default WorkerFeature
+// export default WorkerFeature
